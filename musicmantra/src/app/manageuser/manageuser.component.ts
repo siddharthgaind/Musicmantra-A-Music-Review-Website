220b1480569e2ad1f91a7b25b5264e7277942d83
Siddharth Gaind <sgaind2@uwo.ca>
2019-12-14 15:26:24 +0530

    created frontend to manage users by admin

--- musicmantra/src/app/manageuser/manageuser.component.ts
+++ musicmantra/src/app/manageuser/manageuser.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-manageuser',
@@ -7,9 +8,86 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageuserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  users;
+  response;
+  edit: string;
 
   ngOnInit() {
+    this.http.get('http://localhost:5555/api/admin/getAllUsers', {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        authToken: window.localStorage.getItem('token')
+      })
+    }).subscribe(data => {
+      this.users = data;
+      console.log(this.users);
+    });
   }
 
+  removeUser(userName) {
+    this.http.get('/admin/changeUserType/' + (userName), {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        authToken: window.localStorage.getItem('token')
+      })
+    }).subscribe(data => {
+      this.response = data;
+      console.log(this.response);
+    });
+  }
+  
+  changeUserType( userName, event: any) {
+    const edit = event.target.textContent;  
+    console.log(edit);
+    console.log(userName);
+    var config = {
+      headers: {
+        'Content-Type': 'application/json',
+        'authToken': window.localStorage.getItem('token')
+      }
+    }
+    let post = {
+      userType: edit
+    }
+
+    this.http.put('http://localhost:5555/api/admin/changeUserType/' + userName, JSON.stringify(post), config)
+      .subscribe(data => {
+        this.response = data;
+        console.log(this.response);
+      });
+  }
+
+  changeUserStatus( userName, event: any) {
+    const edit = event.target.textContent;
+    console.log(edit);
+    console.log(userName);
+    var config = {
+      headers: {
+        'Content-Type': 'application/json',
+        'authToken': window.localStorage.getItem('token')
+      }
+    }
+    let post = {
+      userStatus: edit
+    }
+
+    this.http.put('http://localhost:5555/api/admin/changeUserStatus/' + userName, JSON.stringify(post), config)
+      .subscribe(data => {
+        this.response = data;
+        console.log(this.response);
+      });
+  }
+
+  remove(id: any) {
+  }
+
+  add() {
+  }
+
+  changeValue(event: any) {
+    console.log('changeValue');
+    this.edit = event.target.textContent;
+  }
 }
