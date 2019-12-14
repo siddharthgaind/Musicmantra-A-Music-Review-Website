faaf1adc104e8d3816afc67620e3e45617d95024
Siddharth Gaind <sgaind2@uwo.ca>
2019-12-14 13:45:03 +0530

    created frontend to display songs for admin user and remove them

--- musicmantra/src/app/managemusic/managemusic.component.ts
+++ musicmantra/src/app/managemusic/managemusic.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-managemusic',
@@ -7,9 +8,86 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManagemusicComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getMusicforUpdate;
+  scan;
+  getMusic: Array<any> = [];
+  musicAttributes: any = {};
+  output;
+
 
   ngOnInit() {
+    this.http.get('http://localhost:5555/api/secure/getAllMusic', {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        authToken: window.localStorage.getItem('token')
+
+      })
+
+    }).subscribe(data => {
+      let array = [];
+      Object.keys(data).map(function (key) {
+        array.push({ [key]: data[key] })
+      });
+      this.getMusic = array[1].Music;
+      console.log(this.getMusic);
+    });
   }
 
+  updateMusicAttributes(keyField: string, musicName, event: any) {
+    const edit = event.target.textContent;
+    let keyvalue = keyField;
+    var config = {
+      headers: {
+        'Content-Type': 'application/json',
+        'authToken': window.localStorage.getItem('token')
+      }
+    }
+    let post = {
+      [keyField]: edit
+    }
+    this.http.put('http://localhost:5555/api/secure/changeMusicAttribute/' + musicName, JSON.stringify(post), config)
+      .subscribe(data => {
+        this.getMusicforUpdate = data;
+        console.log(this.getMusicforUpdate);
+      });
+  }
+  add() {
+    console.log('add');
+    this.scan = 1;
+    console.log(this.scan);
+  }
+
+  addFieldValue() {
+    console.log(this.musicAttributes);
+    console.log(this.getMusic);
+    this.getMusic.push(this.musicAttributes);
+    var config = {
+      headers: {
+        'Content-Type': 'application/json',
+        'authToken': window.localStorage.getItem('token')
+      }
+    }
+    this.http.post('http://localhost:5555/api/secure/song/', this.musicAttributes, config)
+      .subscribe(data => {
+        this.output = data;
+        console.log(this.output);
+      });
+  }
+
+  remove(index, musicName) {
+    var config = {
+      headers: {
+        'Content-Type': 'application/json',
+        'authToken': window.localStorage.getItem('token')
+      }
+    }
+    this.getMusic.splice(index, 1);
+    this.http.delete('http://localhost:5555/api/admin/deleteMusic/' + musicName, config)
+      .subscribe(data => {
+        this.output = data;
+        console.log(this.output);
+      });
+  }
 }
