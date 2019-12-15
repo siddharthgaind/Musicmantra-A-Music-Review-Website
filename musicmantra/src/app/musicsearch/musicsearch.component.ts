761a3cf197c48515dc60d4c6a63cebbc175f0543
Siddharth Gaind <sgaind2@uwo.ca>
2019-12-15 22:43:58 +0530

    added search functionality

--- musicmantra/src/app/musicsearch/musicsearch.component.ts
+++ musicmantra/src/app/musicsearch/musicsearch.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { GetSet } from 'src/GetSet';
 @Component({
   selector: 'app-musicsearch',
   templateUrl: './musicsearch.component.html',
   styleUrls: ['./musicsearch.component.scss']
 })
 export class MusicsearchComponent implements OnInit {
-
-  constructor() { }
-
+  search: Object;
+  name;
+  constructor(private service: GetSet, public router: Router, private http: HttpClient) {
+    this.service = service;
+  }
   ngOnInit() {
+    this.name = this.service.getData();
+    console.log("Search parameter is " + this.name)
+    this.http.get('http://localhost:5555/api/open/searchMusic/' + this.name).subscribe(data => {
+      this.search = data
+      console.log(this.search);
+    });
   }
 
+  callback() {
+    console.log('Go-back');
+    this.router.navigate(['/']);
+  }
 }
--- musicmantra/src/app/navbar/navbar.component.ts
+++ musicmantra/src/app/navbar/navbar.component.ts
@@ -11,11 +11,10 @@ export class NavbarComponent implements OnInit {
 
   val: string;
 
-  constructor(private router: Router,private service: GetSet) {
+  constructor(private router: Router, private service: GetSet) {
     this.service = service;
   }
   ngOnInit() {
-
   }
   send(str) {
     console.log("string to save is:" + str);
